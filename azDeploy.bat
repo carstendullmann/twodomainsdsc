25d93b88633c43443650defa558d84cbd169498e
Carsten Düllmann <cadull@microsoct.com>
2017-03-12 03:47:40 +0100

    Now with two machines (to become DC)

--- azDeploy.bat
+++ azDeploy.bat
@@ -1,4 +1,5 @@
 REM Requires the xplat python Azure CLI 2.0 installed, see https://docs.microsoft.com/en-us/cli/azure/install-azure-cli and https://docs.microsoft.com/en-us/cli/azure/group/deployment#create
 REM az login
 REM az group create -n twoDoms -l "West Europe"
-az group deployment create --template-file azuredeploy.json -g twoDoms
+REM az group deployment create --template-file azuredeploy.json -g twoDoms
+az group deployment create --template-uri https://raw.githubusercontent.com/carstendullmann/twodomainsdsc/master/azuredeploy.json -g twoDoms
