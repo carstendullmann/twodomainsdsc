41eb0fe0aa95e86df755e5fefd798f28e4588c77
Carsten Düllmann <cadull@microsoct.com>
2017-03-17 21:55:39 +0100

    adding a tfs machine, non domain joined yet.

--- azDeploy.bat
+++
@@ -1,4 +0,0 @@
-REM Requires the xplat python Azure CLI 2.0 installed, see https://docs.microsoft.com/en-us/cli/azure/install-azure-cli and https://docs.microsoft.com/en-us/cli/azure/group/deployment#create
-REM az login
-az group create -n twoDoms -l "West Europe"
-az group deployment create --template-file azuredeploy.json -g twoDoms
